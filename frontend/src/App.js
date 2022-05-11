fed4e34926fa5ae48bf86ff13e6f2dfedaef2f19
Beth Sutton <bksutton2@gmail.com>
2022-05-11 17:05:01 -0600

    react router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,36 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { Container } from 'react-bootstrap';
 // Components
 import Header from './components/Header';
 import Footer from './components/Footer';
+// Pages
 import Home from './pages/Home';
+import Login from './pages/Login';
+import Product from './pages/Product';
 
 function App() {
 	return (
-		<div className='App'>
+		<Router>
 			<Header />
 			<main className='py-3'>
 				<Container>
-					<Home />
+					<Routes>
+						<Route path='/login' element={<Login />} />
+						<Route path='/product/:id' element={<Product />} />
+
+						<Route path='/search/:keyword' element={<Home />} />
+						<Route path='/page/:pageNumber' element={<Home />} />
+						<Route
+							path='/search/:keyword/page/:pageNumber'
+							element={<Home />}
+							exact
+						/>
+						<Route path='/' element={<Home />} />
+					</Routes>
 				</Container>
 			</main>
 			<Footer />
-		</div>
+		</Router>
 	);
 }
 
--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,23 +1,32 @@
 import React from 'react';
+import { Link, Route } from 'react-router-dom';
+import { LinkContainer } from 'react-router-bootstrap';
 import { Container, Navbar, Nav } from 'react-bootstrap';
 
 const Header = () => {
 	return (
 		<header>
-			<Navbar bg='primary' variant='dark' expand='lg' collapseOnSelect>
+			<Navbar bg='success' variant='dark' expand='lg' collapseOnSelect>
 				<Container>
-					<Navbar.Brand href='/'>
-						<i className='fas fa-paw mx-2'></i>Pet Gear
-					</Navbar.Brand>
+					<LinkContainer to='/'>
+						<Navbar.Brand className='fs-3'>
+							<i className='fas fa-paw mx-2 text-primary'></i>
+							Pet Gear
+						</Navbar.Brand>
+					</LinkContainer>
 					<Navbar.Toggle aria-controls='basic-navbar-nav' />
 					<Navbar.Collapse id='basic-navbar-nav'>
 						<Nav className='ms-auto'>
-							<Nav.Link href='/cart'>
-								<i className='fas fa-shopping-cart mx-2'></i>Cart
-							</Nav.Link>
-							<Nav.Link href='/login'>
-								<i className='fas fa-user mx-2'></i>Login
-							</Nav.Link>
+							<LinkContainer to='/cart'>
+								<Nav.Link>
+									<i className='fas fa-shopping-cart mx-2'></i>Cart
+								</Nav.Link>
+							</LinkContainer>
+							<LinkContainer to='/login'>
+								<Nav.Link>
+									<i className='fas fa-user mx-2'></i>Login
+								</Nav.Link>
+							</LinkContainer>
 						</Nav>
 					</Navbar.Collapse>
 				</Container>
