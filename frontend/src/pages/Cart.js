cc37c1e06e035bddf970d834cc632401c7d668d2
Beth Sutton <bksutton2@gmail.com>
2022-05-16 17:16:50 -0600

    cart reducer, add to cart action, cart functionality

--- frontend/src/pages/Cart.js
+++ frontend/src/pages/Cart.js
@@ -1,7 +1,37 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { Link, useParams, useLocation } from 'react-router-dom';
+import { useNavigate } from 'react-router';
+import { useDispatch, useSelector } from 'react-redux';
+import {
+	Row,
+	Col,
+	ListGroup,
+	Image,
+	Form,
+	Button,
+	Card,
+} from 'react-bootstrap';
+import { addToCart } from '../actions/cartActions';
+import Message from '../components/Message';
+import Loader from '../components/Loader';
 
 const Cart = () => {
-	return <div>Cart</div>;
+	const { id } = useParams();
+	const location = useLocation();
+	const qty = Number(new URLSearchParams(location.search).get('qty'));
+
+	const dispatch = useDispatch();
+
+	const cart = useSelector((state) => state.cart);
+	const { cartItems } = cart;
+
+	useEffect(() => {
+		if (id) {
+			dispatch(addToCart(id, qty));
+		}
+	}, [dispatch, id, qty]);
+
+	return <div>{typeof qty}</div>;
 };
 
 export default Cart;
