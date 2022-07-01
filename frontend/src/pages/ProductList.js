073671bc35fd1f3c2474232495f97f203b82627f
Beth Sutton <bksutton2@gmail.com>
2022-06-30 21:04:04 -0600

    delete products

--- frontend/src/pages/ProductList.js
+++ frontend/src/pages/ProductList.js
@@ -6,7 +6,7 @@ import { Table, Button, Row, Col } from 'react-bootstrap';
 import { useDispatch, useSelector } from 'react-redux';
 import Message from '../components/Message';
 import Loader from '../components/Loader';
-import { listProducts } from '../actions/productActions';
+import { listProducts, deleteProduct } from '../actions/productActions';
 
 const ProductList = () => {
 	// const pageNumber = match.params.pageNumber || 1
@@ -18,6 +18,13 @@ const ProductList = () => {
 	const productList = useSelector((state) => state.productList);
 	const { loading, error, products } = productList;
 
+	const productDelete = useSelector((state) => state.productDelete);
+	const {
+		loading: loadingDelete,
+		error: errorDelete,
+		success: successDelete,
+	} = productDelete;
+
 	const userLogin = useSelector((state) => state.userLogin);
 	const { userInfo } = userLogin;
 
@@ -27,15 +34,15 @@ const ProductList = () => {
 		} else {
 			navigate('/login');
 		}
-	}, [dispatch, navigate, userInfo]);
+	}, [dispatch, navigate, userInfo, successDelete]);
 
 	const deleteHandler = (id) => {
 		if (window.confirm('Are you sure')) {
-			// DELETE PRODUCT
+			dispatch(deleteProduct(id));
 		}
 	};
 
-	const createProductHandler = () => {
+	const createProductHandler = (product) => {
 		console.log('create product');
 	};
 
@@ -51,6 +58,8 @@ const ProductList = () => {
 					</Button>
 				</Col>
 			</Row>
+			{loadingDelete && <Loader />}
+			{errorDelete && <Message variant='danger'>{errorDelete}</Message>}
 			{loading ? (
 				<Loader />
 			) : error ? (
